008aaaf3156c5a2fc14c2c67ffb82f7db10fbc15
Caio Tavares <32718663+caioctt@users.noreply.github.com>
2021-04-14 22:56:53 -0300

    Perfil de outro aluno

--- pages/PerfilOutrosAlunos/index.js
+++ pages/PerfilOutrosAlunos/index.js
@@ -1,26 +1,92 @@
 import React from 'react';
 import { SafeAreaView, Image, View } from 'react-native';
-import PercentageBar from '../../components/ExperienceBar/PercentageBar';
+import ButtonOutline from '../../components/ButtonOutlined';
+import ExperienceBar from '../../components/ExperienceBar/index';
 
 import {
-    Scroll,
-    Container,
     Box,
+    Box2,
     ContainerTitleBox,
+    ContainerHeader,
     TitleBox,
-    Title,
-    ImageContainer,
+    TitleBox2,
+    SubTitleBox,
     SubTitle,
-    ProfileImage
+    ProfileImage,
+    SkillBoxContainer,
+    SkillBox,
+    SkillBoxText,
+    Button,
+    ButtonText,
+    ContainerButtons
 } from './styles';
 
 export default function PerfilOutrosAlunos()  {
     return (
-        <View>
-            <Container>
+        <SafeAreaView>
+
+            <ContainerHeader>
                 <ProfileImage source={require('../../assets/profile_3.jpeg')}/>
-            </Container>
-        </View>
+                <Box2>
+                    <ContainerTitleBox>
+                        <TitleBox>
+                            Gabriela Silva
+                        </TitleBox>
+                    </ContainerTitleBox>
+                    <SubTitleBox>
+                        <SubTitle>Está no seu grupo de Gestão do Design </SubTitle>
+                    </SubTitleBox>
+                </Box2>   
+            </ContainerHeader>
+
+            <Box>
+              <ContainerTitleBox>
+                <TitleBox2>
+                    Hard skills
+                </TitleBox2>
+              </ContainerTitleBox>
+                <ExperienceBar name = {"HTML"} width = {9.2}/>
+                <ExperienceBar name = {"Prototipação"} width = {8.8}/>
+                <ExperienceBar name = {"HTML"} width = {9.2}/>
+                <ExperienceBar name = {"Prototipação"} width = {9}/>
+            </Box>
+            <Box>
+              <ContainerTitleBox>
+                <TitleBox>
+                    Soft skills
+                </TitleBox>
+              </ContainerTitleBox>
+              <SkillBoxContainer>
+                <SkillBox>
+                    <SkillBoxText>
+                        Liderança
+                    </SkillBoxText>
+                </SkillBox>
+                <SkillBox>
+                    <SkillBoxText>
+                        Empatia
+                    </SkillBoxText>
+                </SkillBox>
+                <SkillBox>
+                    <SkillBoxText>
+                        Gestão de Conflitos
+                    </SkillBoxText>
+                </SkillBox>
+                <SkillBox>
+                    <SkillBoxText>
+                        Comunicação
+                    </SkillBoxText>
+                </SkillBox>
+              </SkillBoxContainer>
+            </Box>
+            <ContainerButtons>
+                <Button
+                onPress={() => { handleSignIn() }}
+                >
+                <ButtonText>Entrar</ButtonText>
+                </Button>
+            </ContainerButtons>
+        </SafeAreaView>
     );
 }
 
--- pages/PerfilOutrosAlunos/styles.js
+++ pages/PerfilOutrosAlunos/styles.js
@@ -1,48 +1,41 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-    height: auto;
-    flex:1;
-    flex-direction:column;
-    align-items:center;
-    padding:20px 15px 40px 15px;
-    justify-content: space-around;
-`;
-
 export const Box = styled.View`
     width:96%;
     background-color:#fff;
-    padding: 25px 25px 20px 30px;
+    padding: 0px 25px 0px 30px;
+    border-radius: 20px;
+    marginTop: 15px;
+`;
+
+export const Box2 = styled.View`
+    width:96%;
+    background-color:#fff;
+    padding: 10px 5px 30px 20px;
     border-radius: 20px;
     marginBottom: 10px;
     marginTop: 15px;
 `;
 
-export const Title = styled.Text`
-    fontSize: 33px;
-    color: #fff;
-    alignSelf: flex-start;
-    marginTop: 15px;
-    marginBottom: 10px;
-    marginLeft: 30px;
-    fontWeight: bold;
-`
 export const ContainerTitleBox = styled.View`
-    width:100%;
-    flex-direction:row;
-    align-items:center;
+    width: 410px
+    flexDirection:row;
+    alignItems:flex-start;
+    justifyContent: flex-start
+    alignSelf:center;
+    marginTop: 15px;
+    
 `
-
-export const TitleBox = styled.Text`
-    flex:1;
-    fontSize: 20px;
-    color: black;
+export const ContainerHeader = styled.View`
+    width: 410px
+    flexDirection:row;
+    alignItems:flex-start;
+    justifyContent: flex-start
+    alignSelf:center;
+    marginTop: 30px;
+    border-bottom-width:1px;
+    border-bottom-color: #a0a0a0 
 `
-
-export const Scroll = styled.ScrollView`
-    background-color:#3ca1d6;
-`;
-
 
 export const SkillBox = styled.View`
     background-color: #279CDB;
@@ -66,21 +59,60 @@ export const SkillBoxText = styled.Text`
     fontWeight: bold;
 `;
 
-export const ImageContainer = styled.View`
-    flexWrap: wrap;
-    flex-direction:row;
-    width: 100%;
-`;
-
 export const SubTitle = styled.Text`
     color:#828282;
-    marginBottom: 28px;
-    fontSize: 18px;
+    fontSize: 14px;
 `;
+
+export const TitleBox = styled.Text`
+    fontSize: 22px;
+    color: black;
+    alignSelf: flex-start;
+    marginLeft: 20px;
+    marginBottom:5px;
+`
+export const TitleBox2 = styled.Text`
+    fontSize: 22px;
+    color: black;
+    alignSelf: flex-start;
+    marginLeft: 15px;
+    marginBottom:20px;
+`
+
+export const SubTitleBox = styled.View`
+    width:130px
+    alignSelf: flex-start;
+    justifyContent: flex-start;
+    alignItems:flex-start;
+`
 
 export const ProfileImage = styled.Image`
     height: 110px;
     width: 110px;
-    margin-bottom: 20px;
-    border-radius: 55px;
+    marginTop: 20px;
+    borderRadius: 55px;
+    alignItems:flex-start;
+    justifyContent: flex-start;
 `;
+export const Button = styled.TouchableOpacity`
+  width:200px;
+  height: 60px;
+  background-color: ${props => props.invert ? "#fff" : "#ae1b73"};
+  border:1px solid #ae1b73;
+  border-radius:5px;
+  justify-content:center;
+  align-items:center;
+  margin-right:${props => props.invert ? "10px" : "0px"};
+`
+
+export const ButtonText = styled.Text`
+  color:${props => props.invert ? "#ae1b73" : "#fff"};
+  font-size:16px;
+  font-weight:bold;
+`
+export const ContainerButtons = styled.View`
+  flexDirection:row;
+  alignItems: center;
+  justifyContent: center;
+  margin-top:40px;
+`
